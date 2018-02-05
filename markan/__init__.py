3e0d8ba1b8d13d41f2b0f7338ba13ea13b07d807
jcmdln <jcmdln@gmail.com>
2018-02-05 13:41:13 -0500

    Cleanup pass

--- markan/__init__.py
+++ markan/__init__.py
@@ -1,6 +1,3 @@
 import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
-
 from markan.cli import markan
 sys.exit(markan())
--- markan/cmd/reddit.py
+++ markan/cmd/reddit.py
@@ -27,7 +27,7 @@ except ImportError:
 #               help='Target user')
 
 
-def reddit(settings, dictionary, subreddit):
+def reddit(dictionary, settings, subreddit):
     # It is required that the settings file exists in the current
     # directory, so we need to check that it exists and then read any
     # configuration settings.
@@ -42,10 +42,8 @@ def reddit(settings, dictionary, subreddit):
     # We need to check if '-d' was passed to initialize some one-time
     # data to pull the list of words that will be checked against.
     if dictionary:
-        basedict     = config.get('Dictionary', 'base')
-        BaseWords    = set(basedict.split())
-        commentdict  = config.get('Dictionary', 'comment')
-        CommentWords = set(commentdict.split())
+        BaseWords    = config.get('Dictionary', 'base').split()
+        CommentWords = config.get('Dictionary', 'comment').split()
 
     # praw.Reddit is used to initialize the PRAW worker with settings
     # such as the useragent and Oauth2 credentials to use. The
@@ -58,24 +56,29 @@ def reddit(settings, dictionary, subreddit):
     reddit.read_only = True
 
     #
-    for submission in reddit.subreddit(subreddit).hot(limit=None):
+    for submission in reddit.subreddit(subreddit).hot(limit=2):
+        print("reddit: Looking through", subreddit)
         submission = reddit.submission(id=submission.id)
         submission.comments.replace_more(limit=None)
+
         print('--- Submission ---------------------------')
         print('[', 'Score:', submission.score,',',
               'ID:', submission.id, ',',
               'User:', submission.author, ']',
               submission.title)
         print(submission.url)
+
         if dictionary:
             for comment in submission.comments.list():
-                for word in BaseWords and CommentWords:
+                for word in BaseWords:
                     if word in comment.body:
-                        print('--- Comment ---------------------------')
-                        print('[', 'Score:', comment.score,',',
-                              'ID:', comment.id, ',',
-                              'User:', comment.author, ']')
-                        print(comment.body)
+                        for word in CommentWords:
+                            if word in comment.body:
+                                print('--- Comment ---------------------------')
+                                print('[', 'Score:', comment.score,',',
+                                      'ID:', comment.id, ',',
+                                      'User:', comment.author, ']')
+                                print(comment.body)
         else:
             for comment in submission.comments.list():
                 print('--- Comment ---------------------------')
