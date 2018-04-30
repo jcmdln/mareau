3475e8fd9cc11652ae3cd1a52471c35e0c2b652a
jcmdln <jcmdln@gmail.com>
2018-04-30 03:45:22 -0400

    envato.py: Collect more accurate data
    Before I was assuming that I was getting all the data from the category
    I had selected though this was false. Because authors may be marketing
    their themes to multiple categories this created duplicate entries. Now
    I have set the default category to 'wordpress/corporate' which is the
    largest category for wordpress on themeforest, and set the items per
    page to be 100. Because there is a limit of 60 pages and up to 100 items
    per page from the API, you can at most collect 6000 items from the api
    through pagination. Luckily most categories have fewer than half of this
    limit, so my changes allow creating exports of individual categories
    that the user may review and/or combine.

--- markan/cmd/envato.py
+++ markan/cmd/envato.py
@@ -6,21 +6,24 @@ import json as j
 import requests
 
 @click.command(short_help = 'Pull data from the Envato API')
-@click.option('--category', '-c', help='Category',       default='wordpress')
-@click.option('--domain',   '-d', help='Domain',         default='themeforest.net')
-@click.option('--token',    '-t', help='Auth token',     default='')
-@click.option('--csv',      '-c', help='Export as CSV',  is_flag=True, default=False)
-@click.option('--json',     '-j', help='Export as JSON', is_flag=True, default=False)
+@click.option('--category', '-c', help='Category',
+              default='wordpress/corporate')
+@click.option('--domain', '-d', help='Domain', default='themeforest.net')
+@click.option('--token', '-t', help='Auth token', default='')
+@click.option('--csv', '-c', help='Export as CSV', is_flag=True, default=False)
+@click.option('--json', '-j', help='Export as JSON', is_flag=True,
+              default=False)
 
 def envato(category, domain, token, csv, json):
     page = 1
     url  = 'https://api.envato.com/v1/discovery/search/search/item'
-    opts = '?page=' + str(page) + '&site=' + domain + '&category=' + category
+    opts = '?page='+str(page) + '&site='+domain + '&category='+category
     data = []
 
     while url:
-        print('markan: envato: getting page', str(page) + '...')
-        r = requests.get(url + opts, headers = {
+        print('markan: envato: getting page', str(page),
+              'of', domain, category, '...')
+        r = requests.get(url + opts + '&page_size=100', headers = {
             'Content-Type'  : 'application/json',
             'Authorization' : 'Bearer ' + token
         })
@@ -32,7 +35,7 @@ def envato(category, domain, token, csv, json):
             d['id']             = s['matches'][i]['id']
             d['url']            = s['matches'][i]['url']
             d['name']           = s['matches'][i]['name']
-            d['description']    = s['matches'][i]['description']
+            #d['description']    = s['matches'][i]['description']
             d['summary']        = s['matches'][i]['summary']
             d['tags']           = s['matches'][i]['tags']
             d['classification'] = s['matches'][i]['classification']
@@ -49,13 +52,15 @@ def envato(category, domain, token, csv, json):
         pages = s['links']['last_page_url'].split('&')
         pages = pages[1].split('=')
         pages = int(pages[1])
+
         if page == pages:
+            if "/" in category:
+                category = category.split('/')
+                category = category[-1]
             if json:
                 ToJSON('themeforest-'+category+'.json', j.dumps(data))
-
             if csv:
                 ToCSV('themeforest-'+category+'.csv', data)
-
             return
         else:
-            page = page + 1
+            page += 1
